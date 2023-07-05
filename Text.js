2c6b6c0cea340131258140d888ba633f3cd5697d
Hanna Farha <87949041+HannaFarha@users.noreply.github.com>
2023-07-05 21:16:48 +0200

    Add files via upload

---
+++ Text.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Text(){
+    
+    return(
+        <p>React is the best Framework .</p>
+    );
+}
+
+export default Text;
---
+++ memberInfo.js
@@ -0,0 +1,12 @@
+const memberInfo =
+[
+    {id:3, img:'/IMG.jpg',    name:'Johnny Farha',    position:'04/06',           phone:'+49 12345678',   email:'Johnnyfarha@gmail.com',   website:'Fi.com'   },
+    {id:1, img:'/IMG_1603.jpg',    name:'june',    position:'05/10',      phone:'+49 1626203627',   email:'zo@gmail.com',    },
+    {id:2, img:'/IMG_1141.jpg',    name:'Roberto',   position:'09/09',          phone:'+49 12345678',   email:'NR@gmail.com',    website:'Seeme.com'  },
+    
+    {id:4, img:'/IMG_0794.jpg',    name:'Romel',       position:'JJ',  phone:'+49 12345678',   email:'ks@bad.com'                                   },
+    {id:5, img:'/IMG_0191.jpg',    name:'Hoffman G',    position:'ff',        phone:'+49 12345678',   email:'nd.com'                                  },
+   
+]
+
+export default memberInfo
